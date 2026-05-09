da23e04ba47b95d60e1b31fac52783c65165987b
dedsocks <shaunfernsgoa1234@gmail.com>
2026-05-09 23:26:41 +0530

    Adjust window opacity

--- scripts/.local/bin/wallpaper_change.sh
+++ scripts/.local/bin/wallpaper_change.sh
@@ -5,7 +5,7 @@ CACHE_FILE="$HOME/.cache/current_wallpaper"
 
 cd "$WALLPAPER_DIR" || exit 1
 
-SELECTED=$(ls -1 *.jpg *.png *.jpeg 2>/dev/null | rofi -dmenu -i -p "Wallpaper : ")
+SELECTED=$(ls -1 *.jpg *.png *.jpeg *.gif 2>/dev/null | rofi -dmenu -i -p "Wallpaper ")
 
 if [ -z "$SELECTED" ]; then
     exit 0
@@ -13,16 +13,10 @@ fi
 
 TARGET_IMAGE="$WALLPAPER_DIR/$SELECTED"
 
-echo "$TARGET_IMAGE" > "$CACHE_FILE"
-
-POSITIONS=("center" "top" "bottom" "left" "right" "top-left" "top-right" "bottom-left" "bottom-right")
-
-ARRAY_LENGTH=${#POSITIONS[@]}
-RANDOM_INDEX=$(( RANDOM % ARRAY_LENGTH ))
-TRANS_POS=${POSITIONS[$RANDOM_INDEX]}
+ln -sf "$TARGET_IMAGE" "$CACHE_FILE"
 
 awww img "$TARGET_IMAGE" \
     --transition-type grow \
-    --transition-pos "$TRANS_POS" \
+    --transition-pos center \
     --transition-step 90 \
     --transition-fps 60
