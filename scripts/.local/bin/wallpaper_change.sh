d05c0c2fa538ee46768c29445743cdf50b04a787
dedsocks <shaunfernsgoa1234@gmail.com>
2026-05-10 11:16:13 +0530

    Extract frame from gif for hyprlock
    -extract a frame from the wallpaper and store it
    in the cache file
    -this frame will be used by hyprlock during startup
    -ensures consistency between current wallpaper and lockscreen

--- scripts/.local/bin/wallpaper_change.sh
+++ scripts/.local/bin/wallpaper_change.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-CACHE_FILE="$HOME/.cache/current_wallpaper"
+CACHE_DIR="$HOME/.cache/hyprlock"
+CACHE_FILE="$CACHE_DIR/current_wallpaper.png"
 
 cd "$WALLPAPER_DIR" || exit 1
 
@@ -13,10 +14,11 @@ fi
 
 TARGET_IMAGE="$WALLPAPER_DIR/$SELECTED"
 
-ln -sf "$TARGET_IMAGE" "$CACHE_FILE"
-
 awww img "$TARGET_IMAGE" \
     --transition-type grow \
     --transition-pos center \
     --transition-step 90 \
     --transition-fps 60
+
+ffmpeg -y -i "$TARGET_IMAGE" -vframes 1 "$CACHE_FILE" -loglevel error
+
